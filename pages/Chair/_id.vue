d09fd2748747e824f118cbcebccda578a069aa0f
Karolina <karolina.suchom@gmail.com>
2021-01-02 18:00:07 +0100

    adding styles with buttons, working on chair subpages

--- pages/Chair/_id.vue
+++ pages/Chair/_id.vue
@@ -1,31 +1,32 @@
 <template>
-  <div class="chairs">
+  <div class="chair">
     <div class="container">
+      <router-link to="/chairs">
+        <button
+            class="button-back"
+            type="button"
+        >
+          Wróć do listy
+        </button>
+      </router-link>
       <p>
-        Krzesło {{ ' ' + this.$route.params.id }}
+        {{ chair.name }}
       </p>
-      <p>
-        {{ chair }}
-      </p>
+      <img
+          :src="'pictures/chair/' + chair.img"
+          alt="object"
+      >
     </div>
   </div>
 </template>
 
 <script>
-import ObjectsList from '@/components/ObjectsList';
-
 export default {
-  data () {
-    return {
-    };
-  },
   computed: {
     chairs () {
       return this.$store.state.chairs.chairs;
     },
     chair () {
-      const self = this;
-
       return this.$store.getters['chairs/getChair'](parseInt(this.$route.params.id));
     }
   }
