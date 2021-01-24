f68c61e50b8685c6706b63c1997fa2a189b374df
Karolina <karolina.suchom@gmail.com>
2021-01-24 21:51:25 +0100

    work on a shopping cart

---
+++ components/ShoppingCartProducts.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="shopping-cart-table">
+    <PopUpDelete
+        v-if="popup"
+        :id-product="idProduct"
+        @popup="popupValue"
+        @deleteProduct="popupDeleteProduct"
+    />
+    <table>
+      <thead>
+        <tr>
+          <th>
+            Produkt
+          </th>
+          <th>
+            Cena
+          </th>
+          <th>
+            Ilość
+          </th>
+          <th>
+            Razem
+          </th>
+          <th>
+            Usuń
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+            v-for="product in products"
+            :key="product.id"
+        >
+          <td>
+            <router-link
+                class="product-name"
+                :to="'/' + product.species + '/' + product.id"
+            >
+              <div class="product-img">
+                <img
+                    :src="'pictures/' + product.type + '/' + product.img"
+                    alt="product images"
+                >
+              </div>
+              <span>
+                {{ product.name }}
+              </span>
+            </router-link>
+          </td>
+          <td>
+            {{ new Intl.NumberFormat().format(product.price) + ' zł' }}
+          </td>
+          <td>
+            <QuantityProductCounter
+                :counter-value="product.quantity"
+                @counter="counterValue($event, product.id)"
+            />
+          </td>
+          <td>
+            {{ sum (product.price, product.quantity) + ' zł' }}
+          </td>
+          <td>
+            <button
+                class="button-delete"
+                @click="deleteProduct (product.id)"
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import PopUpDelete from '@/components/molecules/PopUpDelete';
+import QuantityProductCounter from '@/components/atoms/QuantityProductCounter';
+
+export default {
+  components: {
+    PopUpDelete,
+    QuantityProductCounter
+  },
+  props: {
+    products: {
+      type: Object,
+      default: null
+    }
+  },
+  data () {
+    return {
+      value: true,
+      popup: false,
+      idProduct: 0,
+      counter: 1
+    };
+  },
+  methods: {
+    sum (price, quantity) {
+      return new Intl.NumberFormat().format(price * quantity);
+    },
+    deleteProduct (id) {
+      this.popup = true;
+      this.idProduct = id;
+    },
+    popupValue (value) {
+      this.popup = value;
+    },
+    popupDeleteProduct (value) {
+      this.$store.dispatch('database/deleteProduct', this.idProduct);
+      this.popup = value;
+    },
+    counterValue (value, id) {
+      this.$store.dispatch('database/changingQuantity', [id, value]);
+    }
+  }
+};
+</script>
--- pages/ShoppingCart.vue
+++ pages/ShoppingCart.vue
@@ -1,11 +1,5 @@
 <template>
   <div class="shopping-cart">
-    <PopUpDelete
-        v-if="popup"
-        :id-product="idProduct"
-        @popup="popupValue"
-        @deleteProduct="popupDeleteProduct"
-    />
     <h3>
       Koszyk
     </h3>
@@ -27,111 +21,31 @@
         v-else
         class="shoping-cart-full"
     >
-      <table>
-        <thead>
-          <tr>
-            <th>
-              Produkt
-            </th>
-            <th>
-              Cena
-            </th>
-            <th>
-              Ilość
-            </th>
-            <th>
-              Razem
-            </th>
-            <th>
-              Usuń
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-              v-for="product in products"
-              :key="product.id"
-          >
-            <td>
-              <router-link
-                  class="product-name"
-                  :to="'/' + product.species + '/' + product.id"
-              >
-                <div class="product-img">
-                  <img
-                      :src="'pictures/' + product.type + '/' + product.img"
-                      alt="product images"
-                  >
-                </div>
-                <span>
-                  {{ product.name }}
-                </span>
-              </router-link>
-            </td>
-            <td>
-              {{ new Intl.NumberFormat().format(product.price) + ' zł' }}
-            </td>
-            <td>
-              <QuantityProductCounter
-                  :counter-value="product.quantity"
-                  @counter="counterValue($event, product.id)"
-              />
-            </td>
-            <td>
-              {{ sum (product.price, product.quantity) + ' zł' }}
-            </td>
-            <td>
-              <button
-                  class="button-delete"
-                  @click="deleteProduct (product.id)"
-              />
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <ShoppingCartProducts
+          :products="products"
+      />
+      <div class="shopping-cart-container">
+        <div>
+          Wysyłka
+        </div>
+        <div>
+          Podsumowanie
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import PopUpDelete from '@/components/molecules/PopUpDelete';
-import QuantityProductCounter from '@/components/atoms/QuantityProductCounter';
+import ShoppingCartProducts from '@/components/ShoppingCartProducts';
 
 export default {
   components: {
-    PopUpDelete,
-    QuantityProductCounter
-  },
-  data () {
-    return {
-      value: true,
-      popup: false,
-      idProduct: 0,
-      counter: 1
-    };
+    ShoppingCartProducts
   },
   computed: {
     products () {
       return this.$store.state.database.shoppingCart;
-    }
-  },
-  methods: {
-    sum (price, quantity) {
-      return new Intl.NumberFormat().format(price * quantity);
-    },
-    deleteProduct (id) {
-      this.popup = true;
-      this.idProduct = id;
-    },
-    popupValue (value) {
-      this.popup = value;
-    },
-    popupDeleteProduct (value) {
-      this.$store.dispatch('database/deleteProduct', this.idProduct);
-      this.popup = value;
-    },
-    counterValue (value, id) {
-      this.$store.dispatch('database/changingQuantity', [id, value]);
     }
   }
 };
